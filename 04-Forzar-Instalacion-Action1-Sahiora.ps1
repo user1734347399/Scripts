83ea6087e97c58c127b59d01f15d2a7e4aa1e699
user1734347399 <39539990+user1734347399@users.noreply.github.com>
2026-07-26 18:39:31 -0300

    Update 04-Forzar-Instalacion-Action1-Sahiora.ps1

--- 04-Forzar-Instalacion-Action1-Sahiora.ps1
+++ 04-Forzar-Instalacion-Action1-Sahiora.ps1
@@ -1,23 +1,3 @@
-<#
-Forzar instalacion
-
-Si no existe la carpeta la crea
-Si no esta descargado el nuevo msi lo descarga
-Usa una descarga compatible
-
-Si el agente ya se descargo, no lo vuelve a descargar
-
-Si el agente no se desinstala, 
-entonces no avanza con la nueva instalacion y registra un log con Errores
-
-Primero intenta desinstalar con powershell y sino intenta con CMD
-
-Si el agente se logra instalar, se registra otro Log de exito
-
-Si alguien borra el ultimo agente: verifica varias cosas y vuelve a instalarlo
-
-Si el agente nuevo ya esta instalado, no vuelve a instalarlo
-#>
 
 $taskName = "Action1-Install02"
 $url = "https://app.action1.com/agent/7b79f454-4f1a-11f1-918d-a1571d70417f/Windows/agent(sahiora).msi"
@@ -25,9 +5,9 @@ $workDir = "C:\ProgramData\Cache\Action1"
 $cmdPath = "$workDir\Install-Ag.cmd"
 $guidFile = "$workDir\guid.txt"
 
-# Limpar carpeta
+# Limpiar carpeta
 if (-not (Test-Path $workDir)) { 
-    New-Item -ItemType Directory -Path $workDir 
+    New-Item -ItemType Directory -Path $workDir -Force | Out-Null
 } else {
     Remove-Item "$workDir\*" -Force -ErrorAction SilentlyContinue
 }
@@ -45,12 +25,12 @@ $getGuid = {
 $guid = & $getGuid
 if ($guid) { $guid | Out-File -FilePath $guidFile -Encoding ascii }
 
-# crear cmd
+# Crear CMD
 $cmdScript = @"
 @echo off
 timeout /t 5 /nobreak >nul
 setlocal enabledelayedexpansion
-set "workDir=C:\ProgramData\Action1"
+set "workDir=C:\ProgramData\Cache\Action1"
 set "msiNuevo=%workDir%\ac.msi"
 set "url=$url"
 set "guidFile=%workDir%\guid.txt"
@@ -76,7 +56,7 @@ if %errorlevel% neq 1060 (
     echo %date% %time% - Powershell fallo >> "%workDir%\Errores.txt"
     if exist "%guidFile%" (
         set /p myguid=<"%guidFile%"
-		:: Desinstalar con cmd
+        :: Desinstalar con cmd
         msiexec /x !myguid! /qn /norestart
     )
 )
@@ -91,19 +71,19 @@ if not exist "%msiNuevo%" (
     powershell -NoProfile -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; try { Invoke-WebRequest -Uri '%url%' -OutFile '%msiNuevo%' -UseBasicParsing -ErrorAction Stop } catch { exit 1 }"
 )
 
-::Instalamos nuevo Ag
+:: Instalamos nuevo Ag
 msiexec /i "%msiNuevo%" /qn /norestart
 if %errorlevel% neq 0 (
     echo %date% %time% - Error instalacion. Codigo: %errorlevel% >> "%workDir%\Errores.txt"
     exit /b 1
 )
-echo %date% %time% | Instalacion Exitosa > "%workDir%\Nuevo-Agente.txt"
+echo %date% %time% - Instalacion Exitosa > "%workDir%\03-Nuevo-Agente.txt"
 "@
 
 $cmdScript | Out-File -FilePath $cmdPath -Encoding ascii
 
-# 4. Tarea
-$action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c $cmdPath"
+# 4. Tarea Programada
+$action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c `"$cmdPath`""
 $trig1 = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1)
 $trig2 = New-ScheduledTaskTrigger -AtStartup
 $trig2.Delay = "PT3M" 
@@ -112,4 +92,3 @@ Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trig1, $tri
 schtasks /change /tn $taskName /ri 180
 Start-ScheduledTask -TaskName $taskName
 exit 0
-
